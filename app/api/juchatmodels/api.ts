1b6d4d9e28ec2a9a6a26dc4751de47dfaa366b5f
ultrasev <i@ultrasev.com>
2024-10-26 14:06:50 +0800

    feat: add logging for JTOKEN and modes

--- app/api/juchatmodels/api.ts
+++ app/api/juchatmodels/api.ts
@@ -1,6 +1,9 @@
 import { ModelInfo, Mode, APIS, HEADERS } from "./types";
+import { addLog } from "@/utils/log";
 
 export async function getModels(): Promise<ModelInfo[]> {
+  await addLog("JTOKEN is " + process.env.JTOKEN?.slice(0, 10), "info");
+
   try {
     const response = await fetch(APIS.MODES, {
       headers: {
@@ -14,8 +17,8 @@ export async function getModels(): Promise<ModelInfo[]> {
     }
 
     const data: any = await response.json();
-    console.log(data);
     const modes: Mode[] = data.data.flatMap((item: any) => item.modes);
+    await addLog(JSON.stringify(modes), "info");
 
     return modes.map((mode) => ({
       id: mode.id.toString(),
